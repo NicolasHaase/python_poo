77e7fc8c8e2e22a66d2f8bda29473318e5d65a23
NicolasHaase <nicolasjimeneshaase@gmail.com>
2024-08-19 21:30:35 -0300

    Simulacao de dois carros com tanque

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from frota import *
 
-def operar_carro(carro : Carro):
+def operar_carro(carro: Carro):
     print('1- Ligar motor')
     print('2- Desligar motor')
     print('3- Acelerar')
@@ -10,32 +10,38 @@ def operar_carro(carro : Carro):
         op = int(input("Digite as opcoes[1-3]: "))
 
     if op == 1:
-        carro1.ligar()
+        carro.ligar()
     elif op == 2:
-        carro1.desligar()
+        carro.desligar()
     elif op == 3:
         v = float(input("Informe a velocidade: "))
         t = float(input("Informe o tempo: "))
-        carro1.acelerar(v, t)
+        carro.acelerar(v, t)
 
 if __name__ == "__main__":
     print('Cadastre o primeiro carro')
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
+    tanque = float(input('Digite a quantidade de litros no tanque: '))
+    consumo = float(input('Digite o consumo médio: '))
 
-    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0, False)
+
+    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0, motor = False, tanque = tanque, consumo_medio = consumo)
 
     print('Cadastre o segundo carro')
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
-    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0, False)
+    tanque = float(input('Digite a quantidade de litros no tanque: '))
+    consumo = float(input('Digite o consumo médio: '))
+
+    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0, motor = False, tanque = tanque, consumo_medio = consumo)
 
     '''
     Controlando o carro até ele atingir 10000 Km
     '''
-    while carro1.odometro < 600 and carro2.odometro < 600:
+    while carro1.odometro < 600 and carro2.odometro < 600 and carro1.tanque > 0 and carro2.tanque > 0:
         try:
             op = 0
             while op not in (1, 2):
@@ -62,6 +68,10 @@ if __name__ == "__main__":
 
     if carro1.odometro >= 600:
         print("O carro 1 ganhou")
-    else:
+    elif carro2.odometro >= 600:
         print("O carro 2 ganhou")
+    elif carro1.tanque == 0:
+        print("Acabou a gasosa do carro 1")
+    else:
+        print("Acabou a gasosa do carro 2")
 
